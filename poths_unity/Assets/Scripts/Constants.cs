14a511cf66881af017a9a0a39b912dbe3cd64b91
Dragonfangs <dragonfang88@hotmail.com>
2015-06-10 15:49:24 +0200

    Add support for simple motions to input reader

--- poths_unity/Assets/Scripts/Constants.cs
+++ poths_unity/Assets/Scripts/Constants.cs
@@ -8,4 +8,10 @@ public static class Constants
 	public const int BUTTONUP =		32;
 	public const int BUTTONDOWN =	64;
 	public const int BUTTONBLOCK =	128;
+	
+	// diagonals
+	public const int BUTTONDOWNRIGHT =	(BUTTONDOWN | BUTTONRIGHT);
+	public const int BUTTONDOWNLEFT =	(BUTTONDOWN | BUTTONLEFT);
+	public const int BUTTONUPRIGHT =	(BUTTONUP | BUTTONRIGHT);
+	public const int BUTTONUPLEFT =		(BUTTONUP | BUTTONLEFT);
 }
--- poths_unity/Assets/Scripts/InputRequirement.cs
+++ poths_unity/Assets/Scripts/InputRequirement.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 public class InputRequirement
 {
@@ -10,40 +11,97 @@ public class InputRequirement
 		inputs.Clear();
 		Input newInput = new Input();
 		
+		bool combine = false;
+		
 		newInput.buttonState = 0;
 		foreach(char c in inputString)
 		{
-			if(c == 'H')
-				newInput.buttonState |= Constants.BUTTONHIGH;
-			if(c == 'M') 
-				newInput.buttonState |= Constants.BUTTONMID;
-			if(c == 'L')
-				newInput.buttonState |= Constants.BUTTONLOW;
-			if(c == '6') 
-				newInput.buttonState |= Constants.BUTTONRIGHT;
-			if(c == '4') 
-				newInput.buttonState |= Constants.BUTTONLEFT;
-			if(c == '8')
-				newInput.buttonState |= Constants.BUTTONUP;
-			if(c == '2')
-				newInput.buttonState |= Constants.BUTTONDOWN;
-			if(c == 'B')
-				newInput.buttonState |= Constants.BUTTONBLOCK;
+			if(combine)
+			{
+				Input toCombine = inputs.Last();
+				toCombine.buttonState |= ParseButton(c);
+				inputs[inputs.Count - 1] = toCombine;
+				combine = false;
+			}
+			else
+			{
+				if(c == '+')
+				{
+					combine = true;
+				}
+				else
+				{
+					newInput.buttonState = ParseButton(c);
+					newInput.leniency = 3;
+					
+					inputs.Add(newInput);
+				}
+			}
 		}
-		
-		newInput.leniency = 0;
-		
-		inputs.Add(newInput);
+	}
+	
+	private int ParseButton(char c)
+	{
+		if(c == 'H')
+			 return Constants.BUTTONHIGH;
+		if(c == 'M') 
+			return Constants.BUTTONMID;
+		if(c == 'L')
+			return Constants.BUTTONLOW;
+		if(c == '1') 
+			return (Constants.BUTTONDOWNLEFT);
+		if(c == '2') 
+			return Constants.BUTTONDOWN;
+		if(c == '3')
+			return Constants.BUTTONDOWNRIGHT;
+		if(c == '4')
+			return Constants.BUTTONLEFT;
+		if(c == '6') 
+			return Constants.BUTTONRIGHT;
+		if(c == '7') 
+			return Constants.BUTTONUPLEFT;
+		if(c == '8')
+			return Constants.BUTTONUP;
+		if(c == '9')
+			return Constants.BUTTONUPRIGHT;
+		if(c == 'B')
+			return Constants.BUTTONBLOCK;
+			
+		return -1;
 	}
 	
 	public bool Verify(Queue<int> buffer)
 	{
-		//UGLY TEMP SOLUTION - Loop through buffer backwards and compare with list of required inputs eventually
-		int buttonState = buffer.ToArray()[buffer.Count - 1];
-		
-		if(this.inputs[0].buttonState == buttonState)
+		//Final button press needs to be pressed on the current frame no matter what
+		if(this.inputs.Last().buttonState != buffer.Last())
+			return false;
+			
+		//If we only have one input, that's it!
+		if(this.inputs.Count == 1)
 			return true;
 			
+		int currentInputIndex = this.inputs.Count - 2; //Last input already confirmed, move to second last
+		int tension = 0; // How many inputs have we skipped looking for the current input. If this exceeds the inputs leniency value, we abort
+		for(int i = buffer.Count - 2; i >= 0; i--)
+		{
+			Input expectedInput = this.inputs[currentInputIndex];
+			int bufferInput = buffer.ToArray()[i];
+			
+			if((bufferInput & expectedInput.buttonState) == expectedInput.buttonState)	//Does expected input exist in the buffer input.
+			{
+				tension = 0;
+				currentInputIndex--;
+				if(currentInputIndex < 0)
+					return true;
+			}
+			else
+			{
+				tension++;
+				if(tension > expectedInput.leniency)
+					return false;
+			}
+		}
+					
 		return false;
 	}
 	
